a018c7c86d243732401cb0953e0ad155a533f223
deancaloracan <dean.caloracan@gmail.com>
2024-08-09 08:38:04 +0800

    remove, edit and mark as complete functions

--- app.py
+++ app.py
@@ -21,5 +21,30 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', todos=todos)
 
+@app.route('/toggle_status/<int:todo_id>')
+def toggle_status(todo_id):
+    for todo in todos:
+        if todo['id'] == todo_id:
+            todo['done'] = not todo['done']
+            break
+    return redirect(url_for('index'))
+
+@app.route('/remove/<int:todo_id>')
+def remove_task(todo_id):
+    global todos # Variable na list
+    todos = [todo for todo in todos if todo['id'] != todo_id] # If you niloop mong todos ay hindi same ng id which we want to delete
+    return redirect(url_for('index'))
+
+@app.route('/edit/<int:todo_id>', methods=['GET', 'POST'])
+def edit_task(todo_id):
+    todo = next((todo for todo in todos if todo['id'] == todo_id), None)
+    if request.method == 'POST':
+        task_description = request.form['edited_todo']
+        if todo and task_description:
+            todo['task_description'] = task_description
+        return redirect(url_for('index'))
+    return render_template('edit_task.html', todo=todo)
+        
+
 if __name__ == '__main__': # Define port, host, and debugging while running
     app.run(debug=True)
